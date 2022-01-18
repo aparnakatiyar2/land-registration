196969117fed6b744467f1479eae4488a78fae86
Aparna Katiyar <aparnakatiyar11@gmail.com>
2022-01-18 20:51:23 +0530

    mid

--- src/Components/Home/Mid-section/MidSection.js
+++ src/Components/Home/Mid-section/MidSection.js
@@ -1,24 +1,54 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import "./MidSection.css"
 
 export const MidSection = () => {
     return (
         <>
-        <div className='Mid-Section'>
-            <div className='Card'>
-                Card1
+            <div class="Mid_section">
+                <div class="Card">
+                    <h3 class="title">Contract Owner</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle class="stroke" cx="60" cy="60" r="50" />
+                        </svg>
+                        <Link className="continue_btn" type="submit">Continue</Link>
+
+                    </div>
+                </div>
+                <div class="Card">
+                    <h3 class="title">Land Inspector</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle class="stroke" cx="60" cy="60" r="50" />
+                        </svg>
+                        <Link className="continue_btn" type="submit">Continue</Link>
+
+                    </div>
+                </div>
+                <div class="Card">
+                    <h3 class="title">User</h3>
+                    <div class="bar">
+                        <div class="emptybar"></div>
+                        <div class="filledbar"></div>
+                    </div>
+                    <div class="circle">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+                            <circle class="stroke" cx="60" cy="60" r="50" />
+                        </svg>
+                            <Link className="continue_btn" type="submit">Continue</Link>
+                    </div>
+                </div>
 
             </div>
-            <div className='Card'>
-                Card2
-
-            </div>
-            <div className='Card'>
-                Card3
-
-            </div>
-
-        </div>
         </>
     )
 }
